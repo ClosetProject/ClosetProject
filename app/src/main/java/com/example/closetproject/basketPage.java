84a3e2c964ba75b9b1f91e91a9e27be105be3a72
smhrd <als0071254@gmail.com>
2022-07-06 21:49:42 +0900

    allcolor추가 main_page.xml 코드 변경 misc. mainPage -> 현재 가장 인기있는 아이템 카드 추가 이미지 추가 ----------------------------------------------- store 페이지에 이미지 추가 aaaa.png figure.xml -> 도형 색깔 변경 mainPage -> 가이드라인 추가 storePage -> 화면구현
    -----------------------------------------------
    [예원]
    스토어페이지 : 장바구니 페이지이동, 스토어list페이지 구현
    쇼핑몰페이지 : 상단 왼쪽 버튼 누를 시 뒤로가기 기능, 장바구니 페이지 기눙, 카테고리 좌우 스크롤 기능, 세부 카테고리 레이아웃 수납기능, 제품은 강제 이미지 구현
    제품상세페이지 : 강제 이미지 구현, 찜 목록(DB로 남어갈 수 있도록), 구매하기(버튼 누르면 옵션창 띄워 색상과 사이즈 받아온 뒤 장바구니, 구매하기 버튼을 이용하여 해당페이지로 이동
    
    저녁 -> 로그인, 회원가입 구현
    
    [민지]
    마이페이지, 장바구니, 주문내역페이지, 주문/결제페이지 구현
    
    png 이미지 추가, 도형(nemo추가), 마이페이지구현
    -> 도형안에 이미지와 텍스트 추가하기
    
    장바구니 listview 생성 -> 0708 어댑터 코드 짜기

---
+++ app/src/main/java/com/example/closetproject/basketPage.java
@@ -0,0 +1,14 @@
+package com.example.closetproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basketPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket_page);
+    }
+}
