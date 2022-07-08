631cc946789f44efd602ab6fa7a275f2fdab548d
leeyewonedu@gmail.com <leeyewonedu@gmai.com>
2022-07-08 21:47:29 +0900

    2022.07.08 21:47 이예원 AnalysisActivity수정 카메라 원본이미지로 받아오기

--- app/src/main/java/com/example/closetproject/AnalysisActivity.java
+++ app/src/main/java/com/example/closetproject/AnalysisActivity.java
@@ -4,18 +4,34 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AnalysisActivity extends AppCompatActivity {
     private ActivityResultLauncher<Intent> resultLauncher;
     private Button btn_runmodel;
+    private ImageView imgViewAnaly;
+
+    private String imageFilePath;
+    private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +39,7 @@ public class AnalysisActivity extends AppCompatActivity {
         setContentView(R.layout.activity_analysis);
 
         btn_runmodel = findViewById(R.id.btn_runmodel);
+        imgViewAnaly = findViewById(R.id.imgViewAnaly);
 
         // intent 양방향 통신 리스너
         resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
@@ -30,24 +47,61 @@ public class AnalysisActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         // Intent 콜백 시, 동작할 함수
+                        if(result.getResultCode() == RESULT_OK){
+                            imgViewAnaly.setImageURI(photoUri);
+                        }
                     }
                 });
 
         btn_runmodel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                sendTakePhotoIntent();
                 // File System
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
+                //Intent intent = new Intent();
+                //intent.setType("image/*");
+                //intent.setAction(Intent.ACTION_GET_CONTENT);
 
                 // Chooser of filesystem optiona
-                Intent chooserIntent = Intent.createChooser(intent,"select source");
+                //Intent chooserIntent = Intent.createChooser(intent,"select source");
 
                 // Add the camera options
-                resultLauncher.launch(Intent.createChooser(intent, "앱을 선택하세요"));
+                //resultLauncher.launch(Intent.createChooser(intent, "앱을 선택하세요"));
             }
         });
     }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "PERSONAL_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg",   /* suffix */
+                storageDir      /* directory */
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void sendTakePhotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            if (photoFile != null) {
+                photoUri = FileProvider.getUriForFile(this, getPackageName(), photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                resultLauncher.launch(takePictureIntent);
+            }
+        }
+    }
+
+
 }
