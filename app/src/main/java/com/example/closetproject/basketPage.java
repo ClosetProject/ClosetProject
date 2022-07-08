26a995f8780b92ea78234718f20f39b45a36eef4
smhrd <als0071254@gmail.com>
2022-07-08 21:45:44 +0900

    allcolor추가 main_page.xml 코드 변경 misc. mainPage -> 현재 가장 인기있는 아이템 카드 추가 이미지 추가 ----------------------------------------------- store 페이지에 이미지 추가 aaaa.png figure.xml -> 도형 색깔 변경 mainPage -> 가이드라인 추가 storePage -> 화면구현
    -----------------------------------------------
    [예원]
    스토어페이지 : 장바구니 페이지이동, 스토어list페이지 구현
    쇼핑몰페이지 : 상단 왼쪽 버튼 누를 시 뒤로가기 기능, 장바구니 페이지 기눙, 카테고리 좌우 스크롤 기능, 세부 카테고리 레이아웃 수납기능, 제품은 강제 이미지 구현
    제품상세페이지 : 강제 이미지 구현, 찜 목록(DB로 남어갈 수 있도록), 구매하기(버튼 누르면 옵션창 띄워 색상과 사이즈 받아온 뒤 장바구니, 구매하기 버튼을 이용하여 해당페이지로 이동
    
    저녁 -> 로그인, 회원가입 구현
    
    [민지]
    마이페이지, 장바구니, 주문내역페이지, 주문/결제페이지 구현
    
    png 이미지 추가, 도형(nemo추가), 마이페이지구현
    -> 도형안에 이미지와 텍스트 추가하기
    
    장바구니 listview 생성 -> 0708 어댑터 코드 짜기 -> 총 금액 추가
    
    장바구니 완료 -> 나중에 한꺼번에 디자인 수정하기
    
    주문내역 페이지 완료 -> 배열에 날짜 별로 넣기
    
    스토어페이지 -> fragment랑 액티비티 연결안되서 보류
    
    주문결게 -> 화면구현완료
    [더보기, 팝업창 띄우기]
    
    로그인페이지 완료

--- app/src/main/java/com/example/closetproject/basketPage.java
+++ app/src/main/java/com/example/closetproject/basketPage.java
@@ -15,7 +15,7 @@ public class basketPage extends AppCompatActivity {
     ListView basket_lv;
     ArrayList<basketVO> data;
     basketCA adapter;
-    String[] s_name = {"커먼유니크", "육육걸즈", "언니날다", "블랙업", "메롱샵", "입어보고"};
+    String[] s_name;
     String[] p_name = {"흰색블라우스", "공주님옷", "샤랄라라", "힙해요", "메롱메롱", "체크무늬크롭"};
     int[] img = {R.drawable.viling, R.drawable.p_jull, R.drawable.p_lrod, R.drawable.p_mall, R.drawable.p_pani, R.drawable.p_tano};
 
