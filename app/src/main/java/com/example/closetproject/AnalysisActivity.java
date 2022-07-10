34f73c1a8199735e0a56a91a4303eaa65696b798
leeyewonedu@gmail.com <leeyewonedu@gmai.com>
2022-07-11 01:47:57 +0900

    2022.07.11 01:47 이예원 카메라, 저장소 권한 허용 코드 추가

--- app/src/main/java/com/example/closetproject/AnalysisActivity.java
+++ app/src/main/java/com/example/closetproject/AnalysisActivity.java
@@ -6,15 +6,19 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -37,6 +41,7 @@ public class AnalysisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis);
+        checkPermission(); // 이중 권한 체크
 
         btn_runmodel = findViewById(R.id.btn_runmodel);
         imgViewAnaly = findViewById(R.id.imgViewAnaly);
@@ -49,6 +54,9 @@ public class AnalysisActivity extends AppCompatActivity {
                         // Intent 콜백 시, 동작할 함수
                         if(result.getResultCode() == RESULT_OK){
                             imgViewAnaly.setImageURI(photoUri);
+                            
+                            // 업로드할 이미지 / 소유자
+                            // File file =
                         }
                     }
                 });
@@ -57,20 +65,13 @@ public class AnalysisActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sendTakePhotoIntent();
-                // File System
-                //Intent intent = new Intent();
-                //intent.setType("image/*");
-                //intent.setAction(Intent.ACTION_GET_CONTENT);
-
-                // Chooser of filesystem optiona
-                //Intent chooserIntent = Intent.createChooser(intent,"select source");
-
-                // Add the camera options
-                //resultLauncher.launch(Intent.createChooser(intent, "앱을 선택하세요"));
             }
         });
     }
-
+    
+    /**
+     * 사진 기능 실행 및 이미지 로컬 저장소에 저장
+     * */
     private File createImageFile() throws IOException{
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "PERSONAL_" + timeStamp + "_";
@@ -92,6 +93,7 @@ public class AnalysisActivity extends AppCompatActivity {
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
+                Log.d("fail to savePhoto", ex.getMessage());
             }
 
             if (photoFile != null) {
@@ -103,5 +105,47 @@ public class AnalysisActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 권한 확인
+     * */
+    private void checkPermission(){
+        int camera_prms = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int wStorage_prms = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int rStorage_prms = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
 
+        // 권한이 열려있는지 확인
+        if (camera_prms == PackageManager.PERMISSION_DENIED || wStorage_prms == PackageManager.PERMISSION_DENIED || rStorage_prms == PackageManager.PERMISSION_DENIED) {
+            // 마쉬멜로우 이상버전부터 권한을 물어본다
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1000);
+            }
+            return;
+        }
+    }
+
+    // 권한 체크 이후로직
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+        if (requestCode == 1000) {
+            boolean check_result = true;
+
+            // 모든 퍼미션을 허용했는지 체크
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            // 권한 체크에 동의를 하지 않으면 안드로이드 종료
+            if (check_result == true) {
+
+            } else {
+                finish();
+            }
+        }
+    }
 }
