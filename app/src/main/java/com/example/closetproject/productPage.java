b5d6ee2e9018866b8c71fbb0d403134aa86a69e3
smhrd <als0071254@gmail.com>
2022-07-13 15:31:06 +0900

    가상결제 시스템 권환 부여 build(project) -> buildscript, allprojects
    build.gradle -> import bootpay
    
    mainfest -> uses 추가 권한부여
    
    쇼핑몰페이지 화면구현 완료
    
    제품 상세페이지 완료
    
    메인페이지 유튜브 연동 완료
    
    마이페이지 화면전환(주문내역 <-> 마이페이지, 장바구니 <-> 마이페이지) 완료

--- app/src/main/java/com/example/closetproject/productPage.java
+++ app/src/main/java/com/example/closetproject/productPage.java
@@ -1,15 +1,22 @@
 package com.example.closetproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class productPage extends AppCompatActivity {
 
     ImageView s_basket3;
+    Button btn_pay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,46 @@ public class productPage extends AppCompatActivity {
         setContentView(R.layout.activity_product_page);
 
         s_basket3 = findViewById(R.id.s_basket3);
+        btn_pay = (Button) findViewById(R.id.btn_pay);
+
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.test_dialog_option, null);
+        final Spinner spSex = (Spinner) alertLayout.findViewById(R.id.spSex);
+        final Spinner spSex2 = (Spinner) alertLayout.findViewById(R.id.spSex2);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("옵션선택");
+        alert.setView(alertLayout);
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String sex = String.valueOf(spSex.getSelectedItem());
+                String sex1 = String.valueOf(spSex2.getSelectedItem());
+
+                Toast.makeText(getBaseContext(), "color: " + sex + "\nsize: " + sex1, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
 
         s_basket3.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/closetproject/test_Dialog.java
+++ app/src/main/java/com/example/closetproject/test_Dialog.java
@@ -11,11 +11,13 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -43,38 +45,11 @@ public class test_Dialog extends AppCompatActivity {
 
         LayoutInflater inflater = getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.test_dialog_option, null);
-        final EditText etName = (EditText) alertLayout.findViewById(R.id.etName);
-        final EditText etDate = (EditText) alertLayout.findViewById(R.id.etDate);
-        final CheckBox cbHungry = (CheckBox) alertLayout.findViewById(R.id.cbHungry);
         final Spinner spSex = (Spinner) alertLayout.findViewById(R.id.spSex);
-        final ImageButton btnDate = (ImageButton) alertLayout.findViewById(R.id.btnDate);
-
-        class SelectDateFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-            @Override
-            public Dialog onCreateDialog(Bundle savedInstanceState) {
-                final Calendar calendar = Calendar.getInstance();
-                int yy = calendar.get(Calendar.YEAR);
-                int mm = calendar.get(Calendar.MONTH);
-                int dd = calendar.get(Calendar.DAY_OF_MONTH);
-                return new DatePickerDialog(getActivity(), this, yy, mm, dd);
-            }
-
-            @Override
-            public void onDateSet(DatePicker view, int yy, int mm, int dd) {
-                etDate.setText((mm + 1) + "/" + dd + "/" + yy);
-            }
-        }
-
-        btnDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment newFragment = new SelectDateFragment();
-                newFragment.show(getSupportFragmentManager(), "DatePicker");
-            }
-        });
+        final Spinner spSex2 = (Spinner) alertLayout.findViewById(R.id.spSex2);
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Form");
+        alert.setTitle("옵션선택");
         alert.setView(alertLayout);
         alert.setCancelable(false);
         alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -87,16 +62,17 @@ public class test_Dialog extends AppCompatActivity {
         alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String user = etName.getText().toString();
-                String bday = etDate.getText().toString();
+
                 String sex = String.valueOf(spSex.getSelectedItem());
-                String hungry = (cbHungry.isChecked()) ? "Yes" : "No";
-                Toast.makeText(getBaseContext(), "Name: " + user + "\nBirthday: " + bday + "\nSex: " + sex + "\nHungry: " + hungry, Toast.LENGTH_SHORT).show();
+                String sex1 = String.valueOf(spSex2.getSelectedItem());
+
+                Toast.makeText(getBaseContext(), "color: " + sex + "\nsize: " + sex1, Toast.LENGTH_SHORT).show();
             }
         });
-        AlertDialog dialog = alert.create();
-        dialog.show();
-}
+
+            AlertDialog dialog = alert.create();
+            dialog.show();
+        }
 
 
     }
