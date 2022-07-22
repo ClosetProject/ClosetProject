05c7a221ad8dba7a6e6e54a1f0c3bb6216288ed7
smhrd <als0071254@gmail.com>
2022-07-22 17:42:59 +0900

    결제페이지

--- app/src/main/java/com/example/closetproject/orderPayPage.java
+++ app/src/main/java/com/example/closetproject/orderPayPage.java
@@ -134,37 +134,6 @@ public class orderPayPage extends AppCompatActivity {
             }
         });
 
-
     }
-
-    //public void onClick_request(View view){
-        // 결제 호출
-        //Bootuser bootuser = new Bootuser().setPhone("010-1234-5678");
-
-//        orderpay_dialog = new Dialog(orderPayPage.this);
-//        orderpay_dialog.getWindow().setGravity(Gravity.BOTTOM);
-//        orderpay_dialog.setContentView(R.layout.activity_orderpay_pay_dialog);
-//
-//
-//        findViewById(R.id.parcel).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                orderpay_dialog.show();
-//                showDialog01();
-//            }
-//        });
-//    }
-//
-//    public void showDialog01(){
-//        orderpay_dialog.show();
-//        TextView tv6 = orderpay_dialog.findViewById(R.id.tv6);
-//        tv6.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                parcel.setText("(CharSequence) tv6");
-//                finish();
-//            }
-//        });
 
 }
