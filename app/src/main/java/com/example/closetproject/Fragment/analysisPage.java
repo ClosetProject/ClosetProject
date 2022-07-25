850d7e9f4aa956f0e1984107ba06c0895e95dbcc
leeyewonedu@gmail.com <leeyewonedu@gmai.com>
2022-07-25 14:52:26 +0900

    분석페이지 데이터 불러오기

--- app/src/main/java/com/example/closetproject/Fragment/analysisPage.java
+++ app/src/main/java/com/example/closetproject/Fragment/analysisPage.java
@@ -19,7 +19,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.closetproject.DTO.DiagnosisDTO;
 import com.example.closetproject.GlobalVariate;
 import com.example.closetproject.MainActivity;
@@ -27,6 +29,7 @@ import com.example.closetproject.R;
 import com.example.closetproject.Retrofit_API.RetrofitClient;
 import com.example.closetproject.Retrofit_API.RetrofitInterface;
 import com.example.closetproject.basketPage;
+import com.example.closetproject.productPage;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -56,8 +59,9 @@ public class analysisPage extends Fragment {
     private HorizontalBarChart barChart;
 
     Button btn_top, btn_b, btn_d, btn_skip;
-    ImageView best_img1, best_img2, best_img3, best_img4;
-    private String m_email, season;
+    private TextView tv_season;
+    private ImageView img_best, img_good, iv_face;
+    private String m_email, season, face_img;
     private RetrofitInterface retrofitAPI;
 
     public static analysisPage newInstance() {
@@ -84,8 +88,12 @@ public class analysisPage extends Fragment {
         btn_top = view.findViewById(R.id.btn_top);
         btn_b = view.findViewById(R.id.btn_b);
         btn_d = view.findViewById(R.id.btn_d);
+        img_best = view.findViewById(R.id.img_best);
+        img_good = view.findViewById(R.id.img_good);
+        iv_face = view.findViewById(R.id.iv_face);
+        tv_season = view.findViewById(R.id.tv_season);
+        barChart = (HorizontalBarChart) view.findViewById(R.id.bar);
 
-            barChart = (HorizontalBarChart) view.findViewById(R.id.bar);
         getDiagnosis();
 
         btn_top.setOnClickListener(new View.OnClickListener(){
@@ -135,8 +143,108 @@ public class analysisPage extends Fragment {
                 @Override
                 public void onResponse(Call<DiagnosisDTO> call, Response<DiagnosisDTO> response) {
                     if(response.isSuccessful()){
-                        //차트
                         DiagnosisDTO diagnosisDTO = response.body();
+                        season = diagnosisDTO.getD_result();
+                        String txt_season = "봄웜";
+                        String file_path = diagnosisDTO.getFace_img();
+                        Glide.with(getContext())
+                                .load(file_path)
+                                .error(R.drawable.noimg)
+                                .into(iv_face);
+
+                        switch (season.substring(0,2)){
+                            case "SW":
+                                img_best.setImageResource(R.drawable.sw_best);
+                                img_good.setImageResource(R.drawable.sw_good);
+                                txt_season = "봄 웜";
+                                break;
+                            case "SC":
+                                img_best.setImageResource(R.drawable.sc_best);
+                                img_good.setImageResource(R.drawable.sc_good);
+                                txt_season = "여름 쿨";
+                                break;
+                            case "AW":
+                                img_best.setImageResource(R.drawable.aw_best);
+                                img_good.setImageResource(R.drawable.aw_good);
+                                txt_season = "가을 웜";
+                                break;
+                            case "WC":
+                                img_best.setImageResource(R.drawable.wc_best);
+                                img_good.setImageResource(R.drawable.wc_good);
+                                txt_season = "겨울 쿨";
+                                break;
+                        }
+
+                        tv_season.setText("당신의 퍼스널 컬러는 " + txt_season + " 입니다");
+
+                        //Chart
+                        ArrayList<BarEntry> feature = new ArrayList<>();
+                        feature.add(new BarEntry(diagnosisDTO.getD_l(),0));
+                        feature.add(new BarEntry(diagnosisDTO.getD_a(),1));
+                        feature.add(new BarEntry(diagnosisDTO.getD_b(),2));
+                        feature.add(new BarEntry(diagnosisDTO.getD_s(),3));
+                        feature.add(new BarEntry(diagnosisDTO.getD_v(),4));
+
+                        ArrayList<String> featureName = new ArrayList<>();
+                        featureName.add("L");
+                        featureName.add("a*");
+                        featureName.add("b*");
+                        featureName.add("S");
+                        featureName.add("V");
+
+                        setData(feature);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<DiagnosisDTO> call, Throwable t) {
+
+                }
+            });
+        }
+    }
+
+    private void getMember(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("m_email", m_email);
+
+        RetrofitClient retrofitClient = RetrofitClient.getInstance();
+        if(retrofitClient != null) {
+            retrofitAPI = RetrofitClient.getRetrofitAPI();
+            retrofitAPI.getAnalysis(params).enqueue(new Callback<DiagnosisDTO>() {
+                @Override
+                public void onResponse(Call<DiagnosisDTO> call, Response<DiagnosisDTO> response) {
+                    if(response.isSuccessful()){
+                        DiagnosisDTO diagnosisDTO = response.body();
+                        season = diagnosisDTO.getD_result();
+                        String txt_season = "봄웜";
+
+                        switch (season.substring(0,2)){
+                            case "SW":
+                                img_best.setImageResource(R.drawable.sw_best);
+                                img_good.setImageResource(R.drawable.sw_good);
+                                txt_season = "봄 웜";
+                                break;
+                            case "SC":
+                                img_best.setImageResource(R.drawable.sc_best);
+                                img_good.setImageResource(R.drawable.sc_good);
+                                txt_season = "여름 쿨";
+                                break;
+                            case "AW":
+                                img_best.setImageResource(R.drawable.aw_best);
+                                img_good.setImageResource(R.drawable.aw_good);
+                                txt_season = "가을 웜";
+                                break;
+                            case "WC":
+                                img_best.setImageResource(R.drawable.wc_best);
+                                img_good.setImageResource(R.drawable.wc_good);
+                                txt_season = "겨울 쿨";
+                                break;
+                        }
+
+                        tv_season.setText("당신의 퍼스널 컬러는 " + txt_season + " 입니다");
+
+                        //Chart
                         ArrayList<BarEntry> feature = new ArrayList<>();
                         feature.add(new BarEntry(diagnosisDTO.getD_l(),0));
                         feature.add(new BarEntry(diagnosisDTO.getD_a(),1));
